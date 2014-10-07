1147cd6d8997f811aa5709d707c52fddd9e980ea
vrojas <vrojas@ec.pe>
2014-10-07 11:54:56 -0500

    sdf

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-//var express = require('express')
-//var app = express();
-
-//app.set('port', (process.env.PORT || 5000))
-//app.use(express.static(__dirname + '/public'))
-
-//app.get('/', function(request, response) {
-//  response.send('Hello World!')
-//})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
-
-
-var express = require('express');
+var express = require('express')
 var app = express();
+
 app.set('port', (process.env.PORT || 5000))
-app.get('/', function(req, res) { 
-	res.send('Geeky Theory probando express.js');
- }); 
-app.get('/prueba', function(req, res) { 
-	res.send('Geeky Theory probando express.js en /prueba'); 
-}); 
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+
+//var express = require('express');
+//var app = express();
+//app.set('port', (process.env.PORT || 5000))
+//app.get('/', function(req, res) { 
+//	res.send('Geeky Theory probando express.js');
+// }); 
+//app.get('/prueba', function(req, res) { 
+//	res.send('Geeky Theory probando express.js en /prueba'); 
+//}); 
+//app.listen(app.get('port'), function() {
+//  console.log("Node app is running at localhost:" + app.get('port'))
+//})
