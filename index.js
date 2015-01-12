4f79f48d9a8c928d70061026ee8d61eba8bd84a5
vrojas <vrojas@ec.pe>
2015-01-12 15:00:00 -0500

    cambios

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-//var express = require('express')
-//var app = express();
-
-//app.set('port', (process.env.PORT || 5000))
-//app.use(express.static(__dirname + '/public'))
-
-//app.get('/', function(request, response) {
-//  response.send('Hello World!')
-//})
-//app.get('/prueba', function(request, response) {
-//  response.send('Geeky Theory probando express.js en /prueba')
-//})
-
-//app.listen(app.get('port'), function() {
-//  console.log("Node app is running at localhost:" + app.get('port'))
-//})
-
-
-//var express = require('express');
-//var app = express();
-//app.set('port', (process.env.PORT || 5000))
-//app.get('/', function(req, res) { 
-//	res.send('Geeky Theory probando express.js');
-// }); 
-//app.get('/prueba', function(req, res) { 
-//	res.send('Geeky Theory probando express.js en /prueba'); 
-//}); 
-//app.listen(app.get('port'), function() {
-//  console.log("Node app is running at localhost:" + app.get('port'))
-//})
-
-
-
 var express = require('express'),
 app = express(),
 server = require('http').createServer(app),
@@ -38,18 +5,19 @@ io = require('socket.io').listen(server);
 
 app.set('port', (process.env.PORT || 5000));
 //app.set('ip', (process.env.IP || '127.0.0.1'));
-app.set('database', (process.env.CLEARDB_DATABASE_UR || 'mysql://bb5dffb23f4faf:94b8c1b2@us-cdbr-iron-east-01.cleardb.net/heroku_cd1d6fb6d0a6d66?reconnect=true'));
+app.set('database', (process.env.CLEARDB_DATABASE_UR || 'mysql://root:123456@localhost/heroku_cd1d6fb6d0a6d66?reconnect=true'));
 
 var url = require('url');
 var parsed = url.parse(app.get('database'));
-console.log(parsed.hostname);
+auth = parsed.auth;
+auth_split = auth.split(":"); 
 
+var mysql = require('mysql');
 
-var mysql = require('mysql');/*
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'me',
-  password : 'secret'
+  host     : parsed.hostname,
+  user     : auth_split['0'],
+  password : auth_split['1']
 });
 
 connection.connect();
@@ -58,9 +26,10 @@ connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
   if (err) throw err;
 
   console.log('The solution is: ', rows[0].solution);
+  mysql = rows[0].solution;
 });
 
-connection.end();*/
+connection.end();
 
 server.listen(app.get('port'));
 
@@ -71,5 +40,6 @@ app.get('/',function(req,res){
 io.sockets.on('connection',function(socket){
     socket.on('sendMessage',function(data){
         io.sockets.emit('newMessage',{msg : data});
+        io.sockets.emit('mysql',{msg : mysql});
     });
 });
